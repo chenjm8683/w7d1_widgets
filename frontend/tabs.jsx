185c53f5ad61eab73dae68cf285136257165cf0c
App Academy Student <student@appacademy.io>
2015-12-07 14:35:00 -0800

    finished tabs

---
+++ frontend/tabs.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+
+var Tabs = React.createClass({
+  getInitialState: function () {
+    return ({tabIndex: 0});
+  },
+  handleClick: function (e) {
+    this.setState({ tabIndex: parseInt(e.target.dataset.tabId) });
+  },
+
+  render: function () {
+    return (
+      <div>
+        <ul>
+          {this.props.ar.map(function(ar, idx) {
+            var isSelected = (idx === this.state.tabIndex)
+              return (
+                <li
+                  key={ar.title}
+                  data-tab-id={idx}
+                  onClick={this.handleClick}
+                  style={isSelected ? {fontWeight: 'bold'} : {}}>
+                  {ar.title}
+                </li>);
+            }.bind(this))
+          }
+        </ul>
+        
+        <p>
+          {this.props.ar[this.state.tabIndex].content}
+        </p>
+
+      </div>
+
+    );
+  }
+});
+
+module.exports = Tabs;
--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Autocomplete = require('./autocomplete.jsx');
 var WeatherClock = require('./weather_clock.jsx');
+var Tabs = require('./tabs.jsx');
 
 var Widgets = React.createClass({
   render: function () {
@@ -11,6 +12,17 @@ var Widgets = React.createClass({
           names={["Colin", "Jacob", "Jeff", "Max", "Ned", "Gizmo"]}
           />
         <WeatherClock />
+        <Tabs
+          ar={
+            [
+            {title: "title 1", content: "content1"},
+            {title: "title 2", content: "content2"},
+            {title: "title 3", content: "content3"},
+            {title: "title 4", content: "content4"},
+            {title: "title 5", content: "content5"}
+            ]
+          }
+        />
       </div>
     );
   }
